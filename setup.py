4ef159890802d4bfdf0ee67a8371d5e33787ec0d
Seehait Chockthanyawat <s.chockthanyawat@gmail.com>
2018-11-03 09:46:35 +0700

    4.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Simple Kubernetes Deployment',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     long_description_content_type='text/markdown',
-    version='4.2.1',
+    version='4.3.0',
     packages=find_packages(),
     url='https://github.com/wongnai/eastern',
     install_requires=[
